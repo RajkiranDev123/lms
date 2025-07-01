850cd4be29ce72363edb21c5656ae643f013f1dd
raj <rajtech645@gmail.com>
2025-07-01 13:22:53 +0530

    redux

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -63,11 +63,66 @@ const authSlice = createSlice({
             state.error = action.payload
 
         },
+        //logout
+        logoutRequest(state) {
+            state.loading = true
+            state.error = null
+            state.message = null
+        },
+        logoutSuccess(state, action) {
+            state.loading = false
+            state.error = action.payload
+            state.isAuthenticated = false
+            state.user = null
+        },
+        logoutFailed(state, action) {
+            state.loading = false
+            state.error = action.payload
+            state.message = null
+        },
+
+        //reset
+        resetAuthSlice(state) {
+            state.loading = false
+            state.error = null
+            state.message = null
+            state.isAuthenticated = state.isAuthenticated
+            state.user = state.user
+        },
+
+        //get user
+        getUserRequest(state) {
+            state.loading = true
+            state.error = null
+            state.message = null
+        },
+        getUserSuccess(state, action) {
+            state.loading = false
+
+            state.isAuthenticated = true
+            state.user = action.payload.user
+        },
+        getUserFailed(state, action) {
+            state.loading = false
+            state.user = null
+            state.isAuthenticated = false
+        },
+        //forgotpassword
+
+
+
+
+
 
 
 
     }
 })
+
+export const resetAuthSlice = (data) => async (dispatch) => {
+    dispatch(authSlice.actions.resetAuthSlice())
+}
+
 
 export const register = (data) => async (dispatch) => {
     dispatch(authSlice.actions.registerRequest())
@@ -97,16 +152,50 @@ export const otpVerification = (email, otp) => async (dispatch) => {
     })
 }
 
-export const login = (email, otp) => async (dispatch) => {
-    dispatch(authSlice.actions.otpVerificationRequest())
-    await axios.post(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/verify-otp`, { email, otp }, {
+export const login = (data) => async (dispatch) => {
+    dispatch(authSlice.actions.loginRequest())
+    await axios.post(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/login`, data, {
         withCredentials: true,
         headers: {
             "Content-Type": "application/json"
         }
     }).then(res => {
-        dispatch(authSlice.actions.otpVerificationSuccess(res.data))
+        dispatch(authSlice.actions.loginSuccess(res.data))
     }).catch(error => {
-        dispatch(authSlice.actions.otpVerificationFailed(error.response.data.message))
+        dispatch(authSlice.actions.loginFailed(error.response.data.message))
     })
 }
+
+export const logout = () => async (dispatch) => {
+    dispatch(authSlice.actions.logoutRequest())
+    await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/logout`, {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => {
+        dispatch(authSlice.actions.loginSuccess(res.data.message))
+        dispatch(authSlice.actions.resetAuthSlice())
+    }).catch(error => {
+        dispatch(authSlice.actions.logoutFailed(error.response.data.message))
+    })
+}
+
+export const getUser = () => async (dispatch) => {
+    dispatch(authSlice.actions.getUserRequest())
+    await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/auth/me`, {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => {
+        dispatch(authSlice.actions.getUserSuccess(res.data))
+
+    }).catch(error => {
+        dispatch(authSlice.actions.getUserFailed(error.response.data.message))
+    })
+}
+
+
+
+
